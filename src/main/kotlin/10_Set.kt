99599f5c86ad9cda285ff5445fd9db5e78464bbb
Nurul Akmal <nakmal618@gmail.com>
2022-03-17 14:20:48 +0700

    contoh coding program Set

--- src/main/kotlin/10_Set.kt
+++ src/main/kotlin/10_Set.kt
@@ -1,5 +1,5 @@
 fun main() {
-//Set<T> stores unique elements; their order is generally undefined.
+//Set<T> stores unique elements; their order is generally undefined
     val numbers = setOf(1, 2, 3, 4)
     println("Number of elements: ${numbers.size}")
     if (numbers.contains(1)) println("1 is in the set")
